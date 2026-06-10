ca0bef7eda436a6b10aeaf0f134c9e89a7fc1371
WyattAu <wyatt_au@protonmail.com>
2026-06-10 09:12:05 +0100

    fix(swift): increase uncompressed budget to 3500MB (actual 3278MB)

--- scripts/build.sh
+++ scripts/build.sh
@@ -169,7 +169,7 @@ case "$FLAVOUR" in
   ruby)           COMP_BUDGET=350; UNCOMP_BUDGET=900 ;;
   php)            COMP_BUDGET=300; UNCOMP_BUDGET=800 ;;
   zig)            COMP_BUDGET=350; UNCOMP_BUDGET=850 ;;
-  swift)          COMP_BUDGET=1100; UNCOMP_BUDGET=2800 ;;
+  swift)          COMP_BUDGET=1100; UNCOMP_BUDGET=3500 ;;
   base-universal) COMP_BUDGET=200; UNCOMP_BUDGET=530 ;;
   *)              COMP_BUDGET=225; UNCOMP_BUDGET=630 ;;
 esac
